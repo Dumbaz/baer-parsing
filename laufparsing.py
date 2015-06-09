cafcfdc62a336aa7f09c1531f5e7d37b7922182e
Dorian <dorian.lenzner@mailbox.tu-dresden.de>
2015-06-09 17:30:51 +0200

    Working State

--- laufparsing.py
+++ laufparsing.py
@@ -4,29 +4,10 @@ import csv
 
 soup = BeautifulSoup(open("Ergebnisliste.xls"), from_encoding="utf-8")
 
-text = soup.get_text()
-
-# print ( text.encode('utf-8') )
-
-rows = soup.findAll('row')
-
-output = io.BytesIO()
-writer = csv.writer(output)
-
-for row in rows:
-	if len(row.text) > 1:
-		writer.writerow( row.text.encode('utf-8') )
-	print output.getvalue()
-
-#writer.writerow(rows.text)
-#output.getvalue()
-
-
-#i = 0
-#for row in rows:
-#	if len(row.text) > 1:
-#		print 'Row %s' %i
-#		print row.text.encode('utf-8')
-#		i = i+1
-
-# print soup.prettify().encode('utf-8')
+for row_tag in soup.findAll('row'):
+	csvelement = row_tag.get_text("|").encode('utf-8'), row_tag.next_sibling
+	if len(str(csvelement)) > 20:
+		s = str(csvelement[0])
+		u = unicode(s, "utf-8")
+		strippedele = "".join(u.split("\n"))
+		print strippedele.encode('utf-8')
