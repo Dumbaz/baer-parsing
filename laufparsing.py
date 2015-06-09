a395e5bc1b1bdb2aa9a5961b0dd8e1a08b9dfe94
Dorian Lenzner <iwantmyfingerback@gmail.com>
2015-06-10 00:38:08 +0200

    FileWrite

--- laufparsing.py
+++ laufparsing.py
@@ -4,10 +4,15 @@ import csv
 
 soup = BeautifulSoup(open("Ergebnisliste.xls"), from_encoding="utf-8")
 
+laufname = soup.find('row').get_text().encode('utf-8')
+
+localFile = open("laufname" + '.csv' , 'wb')
+
 for row_tag in soup.findAll('row'):
 	csvelement = row_tag.get_text("|").encode('utf-8'), row_tag.next_sibling
 	if len(str(csvelement)) > 20:
 		s = str(csvelement[0])
 		u = unicode(s, "utf-8")
 		strippedele = "".join(u.split("\n"))
+		localFile.write(strippedele.encode('utf-8'))
 		print strippedele.encode('utf-8')
