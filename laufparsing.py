2bd0a3e8530c13f7458c453c8526d9077f8692fd
Dorian Lenzner <iwantmyfingerback@gmail.com>
2015-06-09 11:33:54 +0200

    Some changes

--- laufparsing.py
+++ laufparsing.py
@@ -1,7 +1,32 @@
 from bs4 import BeautifulSoup
+import io
+import csv
 
 soup = BeautifulSoup(open("Ergebnisliste.xls"), from_encoding="utf-8")
 
 text = soup.get_text()
 
-print ( text.encode('utf-8') )
+# print ( text.encode('utf-8') )
+
+rows = soup.findAll('row')
+
+output = io.BytesIO()
+writer = csv.writer(output)
+
+for row in rows:
+	if len(row.text) > 1:
+		writer.writerow( row.text.encode('utf-8') )
+	print output.getvalue()
+
+#writer.writerow(rows.text)
+#output.getvalue()
+
+
+#i = 0
+#for row in rows:
+#	if len(row.text) > 1:
+#		print 'Row %s' %i
+#		print row.text.encode('utf-8')
+#		i = i+1
+
+# print soup.prettify().encode('utf-8')
